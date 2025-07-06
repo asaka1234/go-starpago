699dfd1e8733baa293829d3ec838f2287c5c76a8
cyrildou <cyrildou@live.cn>
2025-07-06 16:57:09 +0800

    update entity

--- entity.go
+++ entity.go
@@ -1,6 +1,6 @@
 package go_exglobal
 
-type ExglobalInitParams struct {
+type StarPagoInitParams struct {
 	MerchantId int64  `json:"merchantId" mapstructure:"merchantId" config:"merchantId"  yaml:"merchantId"` // merchantId
 	AccessKey  string `json:"accessKey" mapstructure:"accessKey" config:"accessKey"  yaml:"accessKey"`     //接入秘钥
 	BackKey    string `json:"backKey" mapstructure:"backKey" config:"backKey"  yaml:"backKey"`             //回调秘钥
@@ -11,104 +11,141 @@ type ExglobalInitParams struct {
 	//WithdrawBackUrl string `json:"WithdrawBackUrl" mapstructure:"WithdrawBackUrl" config:"WithdrawBackUrl"  yaml:"WithdrawBackUrl"`
 }
 
-// ----------pre order-------------------------
-
-// 5.2. Create a collection order
-type ExglobalDepositReq struct {
-	MerchantOrderNo  string  `json:"merchantOrderNo" mapstructure:"merchantOrderNo"`   //商户订单号CurrencyCoinName string `json:"currencyCoinName" mapstructure:"currencyCoinName"` //支持VND
-	CurrencyCoinName string  `json:"currencyCoinName" mapstructure:"currencyCoinName"` //支持 VND
-	Amount           float64 `json:"amount" mapstructure:"amount"`                     //不支持小数
-	PaymentMethod    int     `json:"paymentMethod" mapstructure:"paymentMethod"`       //枚举: 1->JSON,3->平台收银台
-	//SDK帮计算
-	//Signature string `json:"signature" mapstructure:"signature"` //签名
-	//UID       int64  `json:"uid" mapstructure:"uid"`             //商户号
-	//BankCode string `json:"bankCode" mapstructure:"bankCode"` //option, 如果用 ScanQRCode, 则这里填 AllBanksSupported
-	//ChannelCode      string  `json:"channelCode" mapstructure:"channelCode"`           //网银扫码:ScanQRCode, 银行直连:BankDirect
+type CommonResp struct {
+	Code  int64  `json:"code" mapstructure:"code"`   // 响应状态码, 0为成功
+	Error string `json:"error" mapstructure:"error"` // 错误信息
+	Msg   string `json:"msg" mapstructure:"msg"`     // 返回文字描述
 }
 
-type ExglobalDepositResponse struct {
-	Code    int    `json:"code" mapstructure:"code"`       // 1是成功
-	Success bool   `json:"success" mapstructure:"success"` //true
-	Message string `json:"message" mapstructure:"message"`
-	Data    struct {
-		UID             int64   `json:"uid" mapstructure:"uid"`                         //商户号
-		MerchantOrderNo string  `json:"merchantOrderNo" mapstructure:"merchantOrderNo"` //商户订单号
-		RecordId        int64   `json:"recordId" mapstructure:"recordId"`               //平台订单号
-		Amount          float64 `json:"amount" mapstructure:"amount"`                   //订单金额
-		URL             string  `json:"url" mapstructure:"url"`                         //收银台地址
-		QRCode          string  `json:"qrcode" mapstructure:"qrcode"`                   //二维码内容
-		MemoCode        string  `json:"memoCode" mapstructure:"memoCode"`               //附言码
-		//以下字段不会返回
-		BankId        *string `json:"bankId" mapstructure:"bankId"`
-		AccountNumber *string `json:"accountNumber" mapstructure:"accountNumber"`
-		BankOwner     *string `json:"bankOwner" mapstructure:"bankOwner"`
-		Signature     string  `json:"signature" mapstructure:"signature"`
-	} `json:"data" mapstructure:"data"`
+// ----------pre order-------------------------
+
+type StarPagoDepositReq struct {
+	Amount     string                  `json:"amount" mapstructure:"amount"`           // 金额
+	Attach     *string                 `json:"attach,omitempty" mapstructure:"attach"` // 附加信息, 商户附加信息，原样返回
+	Currency   string                  `json:"currency" mapstructure:"currency"`       // 金额币种, PKR
+	MerOrderNo string                  `json:"merOrderNo" mapstructure:"merOrderNo"`   // 商户订单号, 商户订单号
+	Extra      StarPagoDepositReqExtra `json:"extra" mapstructure:"extra"`
+	// 以下sdk帮搞
+	// AppID string `json:"appId"` // 应用号, appID
+	// NotifyURL  string                  `json:"notifyUrl"`           // 异步通知地址, 异步通知地址
+	// ReturnURL  *string                 `json:"returnUrl,omitempty"` // 同步通知地址, 同步通知地址
+	// PayMethod  string                  `json:"payMethod"`        // fixed: PK_WALLET
+	// Sign string `json:"sign"` // 签名
+}
+
+type StarPagoDepositReqExtra struct {
+	PayType  string  `json:"payType" mapstructure:"payType"`             // PK_WALLET_JAZZCASH / PK_WALLET_EASYPAISA 其中一个
+	AutoFill *string `json:"autoFill,omitempty" mapstructure:"autoFill"` // 收银台页面是否自动填充提交上来的用户信息(1: 是, 0: 否) 默认值为 0
+	Direct   *string `json:"direct,omitempty" mapstructure:"direct"`     //// 支持用户直连钱包，无需跳转到其他三方收银台(1: 直连, 0: 不直连) 默认值为0
+
+	AccountNo *string `json:"accountNo,omitempty" mapstructure:"accountNo"` // 巴基斯坦的用户cnic: payType 为 PK_WALLET_JAZZCASH 时该项必填
+	Mobile    string  `json:"mobile" mapstructure:"mobile"`                 // 03开头的11位数字（当autoFill="1"或者direct="1"的时候需要传真实的电话号码，否则用户钱包可能收不到支付信息）
+}
+
+// 返回数据
+type StarPagoDepositResponse struct {
+	CommonResp `json:",inline" mapstructure:",squash"`
+
+	Amount     int64  `json:"amount"` // 金额
+	Attach     string `json:"attach"`
+	CreateTime int64  `json:"createTime"`
+	Currency   string `json:"currency"`   // 金额币种, 币种
+	MerOrderNo string `json:"merOrderNo"` // 商户订单号, 商户订单号
+	Message    string `json:"message"`    // 描述信息, 订单异常时会返回此字段，用作异常描述
+	OrderNo    string `json:"orderNo"`    // 交易订单号, 订单号
+	// 订单状态, 支付中 0 1 -4
+	// 支付成功 2 3
+	// 支付失败 -1 -2 -3
+	OrderStatus int64                     `json:"orderStatus"`
+	Params      StarPagoDepositRespParams `json:"params"` // 支付数据
+	Sign        string                    `json:"sign"`   // 签名
+	UpdateTime  int64                     `json:"updateTime"`
+}
+
+// 支付数据
+type StarPagoDepositRespParams struct {
+	BankCode    string `json:"bankCode"`
+	Content     string `json:"content"`     // 支付内容 VA 为数字编号，QRIS 为二维码内容
+	PaymentLink string `json:"paymentLink"` // 收银台地址
+	PayMethod   string `json:"payMethod"`
+	URL         string `json:"url"`
 }
 
 // ------------------------------------------------------------
-type ExglobalDepositBackReq struct {
-	RecordId        int64  `json:"recordId" mapstructure:"recordId"`               //平台订单号
-	UID             int64  `json:"uid" mapstructure:"uid"`                         //商户号
-	OrderAmount     string `json:"orderAmount" mapstructure:"orderAmount"`         //订单金额与下单实际金额一致
-	MerchantOrderNo string `json:"merchantOrderNo" mapstructure:"merchantOrderNo"` //商户订单号
-	TradeStatus     int    `json:"tradeStatus" mapstructure:"tradeStatus"`         //2支付中, 3支付完成, 4支付失败
-	Signature       string `json:"signature" mapstructure:"signature"`
+
+type StarPagoDepositBackReq struct {
+	Amount      int    `json:"amount" mapstructure:"amount"`           //订单金额
+	Attach      string `json:"attach" mapstructure:"attach"`           //附加信息（商户附加信息，原样返回）
+	OrderStatus int    `json:"orderStatus" mapstructure:"orderStatus"` //	订单状态支付中 0 1 -4, 支付成功 2 3, 支付失败 -1 -2 -3
+	OrderNo     string `json:"orderNo" mapstructure:"orderNo"`         //交易订单号
+	MerOrderNo  string `json:"merOrderNo" mapstructure:"merOrderNo"`   //商户订单号
+	Currency    string `json:"currency" mapstructure:"currency"`       //金额币种
+	Message     string `json:"message" mapstructure:"message"`         //描述信息（订单异常时会返回此字段，用作异常描述）
+	CreateTime  int64  `json:"createTime" mapstructure:"createTime"`   //毫秒, 1663236430613
+	UpdateTime  int64  `json:"updateTime" mapstructure:"updateTime"`   //毫秒, 1663236430613
+	Sign        string `json:"sign" mapstructure:"sign"`
 }
 
-type ExglobalDepositBackResp struct {
-	Code    int    `json:"code" mapstructure:"code"` // 1是成功
-	Success bool   `json:"success" mapstructure:"success"`
-	Message string `json:"message" mapstructure:"message"`
-	Data    string `json:"data" mapstructure:"data"`
-}
+// response 是 succeess / ok
 
 //===========withdraw===================================
 
-type ExglobalWithdrawReq struct {
-	MerchantOrderNo  string `json:"merchantOrderNo" mapstructure:"merchantOrderNo"`   //商户订单号
-	CurrencyCoinName string `json:"currencyCoinName" mapstructure:"currencyCoinName"` //VND
-	Amount           string `json:"amount" mapstructure:"amount"`                     //不支持小数
-	BankCode         string `json:"bankCode" mapstructure:"bankCode"`
-	BankName         string `json:"bankName" mapstructure:"bankName"`
-	BankBranchName   string `json:"bankBranchName" mapstructure:"bankBranchName"`
-	BankUserName     string `json:"bankUserName" mapstructure:"bankUserName"`
-	BankAccount      string `json:"bankAccount" mapstructure:"bankAccount"`
-	//以下sdk帮搞
-	//UID       int64  `json:"uid" mapstructure:"uid"`             //商户编码
-	//ChannelCode      string `json:"channelCode" mapstructure:"channelCode"` //写死 BankDirect
-	//Signature string `json:"signature" mapstructure:"signature"` //签名
+type StarPagoWithdrawReq struct {
+	Amount     string                   `json:"amount" mapstructure:"amount"`         // 金额
+	Attach     string                   `json:"attach" mapstructure:"attach"`         // 附加信息, 商户附加信息，原样返回
+	Currency   string                   `json:"currency" mapstructure:"currency"`     // 金额币种, PKR
+	Extra      StarPagoWithdrawReqExtra `json:"extra" mapstructure:"extra"`           // 扩展信息
+	MerOrderNo string                   `json:"merOrderNo" mapstructure:"merOrderNo"` // 商户订单号, 商户订单号
+	//以下sdk设置
+	//AppID string `json:"appId"` // 应用号, appID
+	//NotifyURL string `json:"notifyUrl"` // 异步通知地址, 异步通知地址
+	//Sign  string `json:"sign"`  // 签名
+	//PayMethod  string                   `json:"payMethod"`  // 写死的: PK_DF
 }
 
-type ExglobalWithdrawResponse struct {
-	Code    int    `json:"code" mapstructure:"code"`       // 1是成功
-	Success bool   `json:"success" mapstructure:"success"` //true
-	Message string `json:"message" mapstructure:"message"`
-	Data    struct {
-		Uid              int64  `json:"uid" mapstructure:"channelCode"`                 //商户编号
-		MerchantOrderNo  string `json:"merchantOrderNo" mapstructure:"merchantOrderNo"` //商户订单号
-		RecordId         int64  `json:"recordId" mapstructure:"recordId"`               //平台订单号
-		Amount           string `json:"amount" mapstructure:"amount"`
-		CurrencyCoinName string `json:"currencyCoinName" mapstructure:"currencyCoinName"` //VND
-		BankName         string `json:"bankName" mapstructure:"bankName"`
-		BankBranchName   string `json:"bankBranchName" mapstructure:"bankBranchName"`
-		BankUserName     string `json:"bankUserName" mapstructure:"bankUserName"`
-		BankAccount      string `json:"bankAccount" mapstructure:"bankAccount"`
-	} `json:"data" mapstructure:"data"`
+// 扩展信息
+type StarPagoWithdrawReqExtra struct {
+	// 持卡人姓名, 收款人姓名(尽量真实)
+	AccountName *string `json:"accountName,omitempty" mapstructure:"accountName"`
+	// 收款银行账号(银行卡代付校验真实性，否则可能出款到错误的用户身上)
+	AccountNo string `json:"accountNo" mapstructure:"accountNo"`
+	// 参考代付银行对照表
+	BankCode string `json:"bankCode" mapstructure:"bankCode"`
+	// 巴基斯坦必填，身份证号13位纯数字（EP不交易真实,JZ校验） 尽量真实
+	Cnic string `json:"cnic" mapstructure:"cnic"`
+	// 收款人邮箱（格式正确就行）
+	Email *string `json:"email,omitempty" mapstructure:"email"`
+	// 收款人电话 (校验真实性，03开头11位或者3开头10位数)钱包代付时必须真实，否则可能出现出款到错误的用户钱包去
+	Mobile string `json:"mobile" mapstructure:"mobile"`
 }
 
-type ExglobalWithdrawBackReq struct {
-	RecordId        int64  `json:"recordId" mapstructure:"recordId"`               //平台订单号
-	UID             int64  `json:"uid" mapstructure:"uid"`                         //商户号
-	OrderAmount     string `json:"orderAmount" mapstructure:"orderAmount"`         //订单金额与下单实际金额一致
-	MerchantOrderNo string `json:"merchantOrderNo" mapstructure:"merchantOrderNo"` //商户订单号
-	TradeStatus     int    `json:"tradeStatus" mapstructure:"tradeStatus"`         //2支付中, 3支付完成, 4支付失败
-	Signature       string `json:"signature" mapstructure:"signature"`
+// 返回数据
+type StarPagoWithdrawResponse struct {
+	CommonResp `json:",inline" mapstructure:",squash"`
+
+	Amount     int64  `json:"amount"`     // 订单金额
+	Currency   string `json:"currency"`   // 金额币种
+	MerOrderNo string `json:"merOrderNo"` // 商户订单号
+	Message    string `json:"message"`    //// 描述信息, 订单异常时会返回此字段，用作异常描述
+	OrderNo    string `json:"orderNo"`    // 交易订单号
+	// 订单状态, 支付中 0 1 -4
+	// 支付成功 2 3
+	// 支付失败 -1 -2 -3
+	OrderStatus int64  `json:"orderStatus"`
+	Sign        string `json:"sign"` // 签名
+	CreateTime  int64  `json:"createTime"`
+	UpdateTime  int64  `json:"updateTime"`
 }
 
-type ExglobalWithdrawBackResp struct {
-	Code    int    `json:"code" mapstructure:"code"` // 1是成功
-	Success bool   `json:"success" mapstructure:"success"`
-	Message string `json:"message" mapstructure:"message"`
-	Data    string `json:"data" mapstructure:"data"`
+type StarPagoWithdrawBackReq struct {
+	OrderStatus int    `json:"orderStatus" mapstructure:"orderStatus"` //订单状态（订单状态支付中 0 1 -4, 支付成功 2 3, 支付失败 -1 -2 -3
+	Attach      string `json:"attach" mapstructure:"attach"`           //附加信息（商户附加信息，原样返回）
+	OrderNo     string `json:"orderNo" mapstructure:"orderNo"`         //交易订单号
+	MerOrderNo  string `json:"merOrderNo" mapstructure:"merOrderNo"`   //商户订单号
+	Amount      int    `json:"amount" mapstructure:"amount"`           //订单金额
+	Currency    string `json:"currency" mapstructure:"currency"`       //金额币种
+	Message     string `json:"message" mapstructure:"message"`         //描述信息（订单异常时会返回此字段，用作异常描述）
+	CreateTime  int64  `json:"createTime" mapstructure:"createTime"`   //毫秒, 1663236430613
+	UpdateTime  int64  `json:"updateTime" mapstructure:"updateTime"`   //毫秒, 1663236430613
+	Sign        string `json:"sign" mapstructure:"sign"`
 }
